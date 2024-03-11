6196ffb2104c52c0820977f468b7c9b196b3e946
Ethan Cohen <ecohen56@gmail.com>
2024-03-11 10:26:53 -0400

    Basic example from site

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
+import { AssemblyAI } from "assemblyai";
+
 const main = async (): Promise<void> => {
-    console.log("Hello, world!");
+    const client = new AssemblyAI({
+        apiKey: "e0fec45b18bf42d3ad1da58bb202d3b4",
+    });
+
+    const FILE_URL =
+        "https://storage.googleapis.com/aai-web-samples/5_common_sports_injuries.mp3";
+
+    const data = {
+        audio_url: FILE_URL,
+    };
+
+    const transcript = await client.transcripts.transcribe(data);
+    console.log(transcript.text);
 };
 
 main().catch((error) => {
